3b6a7b83159af91c169c4b894c12e5616d387395
brook <81716228+brookhhh@users.noreply.github.com>
2022-07-28 07:05:54 +0100

    uhh its better now
    jank code

--- run.py
+++ run.py
@@ -2,7 +2,9 @@ import time
 import discord.ext
 import random
 
-token: str = "put token here"
+token: str = "put token here"  # ez
+delay = input("enter your delay\n")  # this is not jank
+prefix = input("enter your prefix\n")  # and this is jank (it throws so many errors to me for no reason)
 
 
 class MyClient(discord.Client):
@@ -11,34 +13,31 @@ class MyClient(discord.Client):
     time.sleep(1.5)
     print("not really recommended to run as admin but it doesnt really matter")
     time.sleep(1)
-    option1 = input("put secret to run this...")
+    option1 = input("put 'secret' to run this...")
 
-    # actual self bot bit of code ahead
+    # actual self bot bit of code
     if option1 == "secret":
-        print("juice wrld unbelieveaboat selfbot work work work deposit! (made by brook)")
-        time.sleep(1.5)
-        print("your money will probably be beamed by expando if you do this, but that's not my fault")
+        print("juice wrld cyan selfbot work work work deposit! (made by brook)")
         time.sleep(1.5)
         print("when you want it to start just type start in the channel you want to selfbot in")
 
         class MyClient(discord.Client):
+
             async def on_ready(self):
-                print('logged on account', self.user, 'ready to bot')
+                print('logged on account, ready to bot')
 
         async def on_message(self, message):
-            # only respond to ourselves
             if message.author != self.user:
                 return
 
             if message.content == 'start':
                 while 6 > 4:
-                    
-                    identifier=random.uniform(1,9000)
-                    await message.channel.send('!work')  # work
+                    identifier = random.uniform(1, 9000)
+                    await message.channel.send(prefix + 'work')  # https://www.reddit.com/r/introvert/comments/3ewxpe/how_many_of_you_introverts_just_work_sleep_eat/
                     time.sleep(0.7)
-                    await message.channel.send('!dep all')  # https://tenor.com/view/kitsune-shy-anime-gif-17046222
-                    print("cycle id number", identifier, "done, sleeping for 30s\n") # id system jamk, its just to let you know its working
-                    time.sleep(29.5)
+                    await message.channel.send(prefix + 'dep all')  # imagine deposit as eating
+                    print("cycle id number", identifier, "done, sleeping for", delay, "s\n")  # sometimes this will throw an error for some reason
+                    time.sleep(int(delay))
 
 
 client = MyClient()
